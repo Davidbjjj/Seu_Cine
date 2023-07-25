eb0d5b60700707b478a8afb598064ff7a8e7f384
Davidbjjj <60227838+Davidbjjj@users.noreply.github.com>
2023-07-25 20:37:48 -0300

    adicionando carrossel dos filmes:✔

---
+++ js/index.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper('.swiper-container',{
+  slidesPerView:5,
+  slidesPerGroup:4,
+  spaceBetween:10,
+  navigation:{
+    nextEl:'.swiper-button-next',
+    prevEl:'.swiper-button-prev',
+  },
+});
